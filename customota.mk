a4c77ff7949bf766f2e686d81f8f6c90de71cace
mickybart <mickybart@pygoscelis.org>
2014-09-14 11:21:26 -0400

    OTA support + Recovery set to use FOTAkernel only

--- customota.mk
+++ customota.mk
@@ -11,11 +11,11 @@ INTERNAL_OTA_PACKAGE_TARGET := $(PRODUCT_OUT)/$(name).zip
 $(INTERNAL_OTA_PACKAGE_TARGET): KEY_CERT_PAIR := $(DEFAULT_KEY_CERT_PAIR)
 
 $(INTERNAL_OTA_PACKAGE_TARGET): $(BUILT_TARGET_FILES_PACKAGE) $(DISTTOOLS)
-        @echo "Package OTA: $@"
-        $(hide) MKBOOTIMG=$(BOARD_CUSTOM_BOOTIMG_MK) \
+	@echo "Package OTA: $@"
+	$(hide) MKBOOTIMG=$(BOARD_CUSTOM_BOOTIMG_MK) \
 	   ./build/tools/releasetools/ota_from_target_files -v \
-           -p $(HOST_OUT) \
-           -k $(KEY_CERT_PAIR) \
-           --no_separate_recovery=true \
-           $(BUILT_TARGET_FILES_PACKAGE) $@
+	   -p $(HOST_OUT) \
+	   -k $(KEY_CERT_PAIR) \
+	   --no_separate_recovery=true \
+	   $(BUILT_TARGET_FILES_PACKAGE) $@
 
