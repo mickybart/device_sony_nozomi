01d6582667d5286f17a2a5ee6bd3706667d46363
mickybart <mickybart@pygoscelis.org>
2015-01-08 14:05:41 -0500

    liblight: brightness support for button-backlight

--- liblight/lights.c
+++ liblight/lights.c
@@ -125,9 +125,9 @@ static int set_light_backlight (struct light_device_t *dev, struct light_state_t
 }
 
 static int set_light_buttons (struct light_device_t *dev, struct light_state_t const* state) {
-	int on = is_lit(state);
+	int brightness = rgb_to_brightness(state);
 	pthread_mutex_lock(&g_lock);
-	write_int (BUTTON_BACKLIGHT_FILE, on ? 255 : 0);
+	write_int (BUTTON_BACKLIGHT_FILE, brightness);
 	pthread_mutex_unlock(&g_lock);
 	return 0;
 }
