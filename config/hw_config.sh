6333fbe73a9f3b8af93c7f9408c5a2bed90afa6f
mickybart <mickybart@pygoscelis.org>
2015-01-27 10:40:01 -0500

    shake2w: disable by default

--- config/hw_config.sh
+++ config/hw_config.sh
@@ -51,5 +51,5 @@ echo 1 > /sys/class/input/input3/wakeup
 # bma250 motion
 dev=/sys/class/input/input6
 echo 0 0 0 20 > $dev/tap_param
-echo 130 > $dev/tap_mode
+#echo 130 > $dev/tap_mode
 echo 3 48 > $dev/slope_param
