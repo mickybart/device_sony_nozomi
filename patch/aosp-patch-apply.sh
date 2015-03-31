278377963e1b9684f7198e88f28855a0e8da5834
mickybart <mickybart@pygoscelis.org>
2015-03-31 19:39:36 -0400

    patch : tools to patch/unpatch/generate control file

---
+++ patch/aosp-patch-apply.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+AOSP=$1
+PATCHES="aosp-patch-md5.lst"
+
+function die {
+        echo "ERROR - ${1-UNKNOWN}" >&2
+        exit 1
+}
+
+function usage {
+        cat << EOF
+usage : $0 aosp
+ aosp : path to your local aosp repo sync
+
+EOF
+        exit 2
+}
+
+# parameters
+if [ $# -ne 1 ]; then
+        usage
+fi
+
+[ ! -f $PATCHES ] && die "file $PATCHES not found"
+
+while read -r hashaosp hashnew filename
+do
+	if [ -f $AOSP/$filename ]; then
+		hash=$(md5sum $AOSP/$filename | awk '{print $1}')
+	else
+		hash="--------------------------------"
+	fi
+
+	if [ "$hash" == "$hashaosp" ]; then
+		mkdir -p $AOSP/$(dirname "$filename")
+		cp -af $filename $AOSP/$filename || die "failed to copy $filename into $AOSP/$filename"
+	else
+		if [ "$hash" != "$hashnew" ]; then
+			die "patch $filename is not valid in this context. please merge manually"
+		fi
+	fi
+done < $PATCHES
+
---
+++ patch/aosp-patch-unapply.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+AOSP=$1
+PATCHES="aosp-patch-md5.lst"
+
+function die {
+        echo "ERROR - ${1-UNKNOWN}" >&2
+        exit 1
+}
+
+function usage {
+        cat << EOF
+usage : $0 aosp
+ aosp : path to your local aosp repo sync
+
+EOF
+        exit 2
+}
+
+# parameters
+if [ $# -ne 1 ]; then
+        usage
+fi
+
+[ ! -f $PATCHES ] && die "file $PATCHES not found"
+
+while read -r hashaosp hashnew filename
+do
+	if [ "$hashaosp" == "--------------------------------" ]; then
+		if [ -f $AOSP/$filename ]; then
+			rm -f $AOSP/$filename || die "failed to remove $AOSP/$filename"
+		fi
+	else
+		cat << EOF | bash
+		cd $AOSP/$(dirname "$filename")
+		git checkout -- $(basename "$filename") || echo "failed to checkout $filename" >&2
+EOF
+	fi
+done < $PATCHES
+
