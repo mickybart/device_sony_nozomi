b09a3d71c05c039fe1b192a42779e16eef999e0a
mickybart <mickybart@pygoscelis.org>
2015-05-09 08:07:37 -0400

    property: disable data roaming by default

--- full_nozomi.mk
+++ full_nozomi.mk
@@ -34,6 +34,10 @@ PRODUCT_MODEL := Xperia S
 PRODUCT_MANUFACTURER := Sony
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
+#Disable Data roaming by default
+PRODUCT_PROPERTY_OVERRIDES := \
+    ro.com.android.dataroaming=false
+
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/sony/nozomi/device.mk)
 $(call inherit-product-if-exists, vendor/sony/nozomi/device-vendor.mk)
