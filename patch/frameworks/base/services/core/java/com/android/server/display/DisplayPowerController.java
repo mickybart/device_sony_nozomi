49c6f2cd98c4fe60521f0f24299ccf88a47b75a6
mickybart <mickybart@pygoscelis.org>
2015-05-21 19:51:55 -0400

    patch: doze: support adaptive

--- patch/frameworks/base/services/core/java/com/android/server/display/DisplayPowerController.java
+++ patch/frameworks/base/services/core/java/com/android/server/display/DisplayPowerController.java
@@ -278,8 +278,15 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
         final int screenBrightnessSettingMinimum = clampAbsoluteBrightness(resources.getInteger(
                 com.android.internal.R.integer.config_screenBrightnessSettingMinimum));
 
+        // Doze brightness custom
         int screenBrightnessDozeProperty = SystemProperties.getInt("persist.screen.doze_brightness",-1);
-        if (screenBrightnessDozeProperty < 0 || screenBrightnessDozeProperty > 255) {
+        if (screenBrightnessDozeProperty == 0) {
+            mAllowAutoBrightnessWhileDozingConfig = true;
+        } else {
+            mAllowAutoBrightnessWhileDozingConfig = resources.getBoolean(
+                    com.android.internal.R.bool.config_allowAutoBrightnessWhileDozing);
+        }
+        if (mAllowAutoBrightnessWhileDozingConfig || screenBrightnessDozeProperty < 0 || screenBrightnessDozeProperty > 255) {
             screenBrightnessDozeProperty = resources.getInteger(
                     com.android.internal.R.integer.config_screenBrightnessDoze);
         }
@@ -310,9 +317,6 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
 
         mUseSoftwareAutoBrightnessConfig = resources.getBoolean(
                 com.android.internal.R.bool.config_automatic_brightness_available);
-
-        mAllowAutoBrightnessWhileDozingConfig = resources.getBoolean(
-                com.android.internal.R.bool.config_allowAutoBrightnessWhileDozing);
 
         if (mUseSoftwareAutoBrightnessConfig) {
             String luxStr = SystemProperties.get("persist.screen.auto_lux","");
