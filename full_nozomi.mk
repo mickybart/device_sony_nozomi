6388791993100bb7ac2ee4f3726915181d2a9674
mickybart <mickybart@pygoscelis.org>
2015-07-01 13:53:44 -0400

    audio_effects.xml: change inclusion order

--- full_nozomi.mk
+++ full_nozomi.mk
@@ -23,6 +23,9 @@ PRODUCT_PACKAGES += \
 # Get the long list of APNs
 PRODUCT_COPY_FILES += device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
 
+# Override the audio_effects.conf
+PRODUCT_COPY_FILES += device/sony/nozomi/config/audio_effects.conf:system/etc/audio_effects.conf
+
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
