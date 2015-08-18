df0ed126058249c7e2cfadb98c9075438d74fd9c
mickybart <mickybart@pygoscelis.org>
2015-08-18 10:06:06 -0400

    ota: with block

--- custom/customota.mk
+++ custom/customota.mk
@@ -14,6 +14,7 @@ $(INTERNAL_OTA_PACKAGE_TARGET): $(BUILT_TARGET_FILES_PACKAGE) $(DISTTOOLS)
 	@echo "Package OTA: $@"
 	$(hide) MKBOOTIMG=$(BOARD_CUSTOM_BOOTIMG_MK) \
 	   ./build/tools/releasetools/ota_from_target_files -v \
+	   --block \
 	   -p $(HOST_OUT) \
 	   -k $(KEY_CERT_PAIR) \
 	   --no_separate_recovery=true \
