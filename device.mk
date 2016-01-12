89cdfef322a2d7759e934db18cfba7ead2f7bd24
mickybart <mickybart@pygoscelis.org>
2016-01-12 09:19:39 -0500

    FMRadio is back

--- device.mk
+++ device.mk
@@ -199,11 +199,9 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     com.android.future.usb.accessory
 
-## Fm radio
-#PRODUCT_PACKAGES += \
-#    com.stericsson.hardware.fm \
-#    com.stericsson.hardware.fm.xml \
-#    FMRadio
+# Fm radio
+PRODUCT_PACKAGES += \
+    FMRadio
 
 # Key layouts and touchscreen
 PRODUCT_COPY_FILES += \
