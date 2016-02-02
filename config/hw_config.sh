125f13de4d8d9abd601568920f2b7a84f88d48cd
mickybart <mickybart@pygoscelis.org>
2016-02-02 13:12:28 +0100

    hw_config: bma250 motion moved to input7

--- config/hw_config.sh
+++ config/hw_config.sh
@@ -50,7 +50,7 @@ bq275xx_fwloader
 #echo 1 > /sys/class/input/input3/wakeup
 
 # bma250 motion
-dev=/sys/class/input/input6
+dev=/sys/class/input/input7
 echo 0 0 0 20 > $dev/tap_param
 #echo 130 > $dev/tap_mode
 echo 3 48 > $dev/slope_param
