0ea78129fbe3d30bf4963da7f8eda79f84f6b404
mickybart <mickybart@pygoscelis.org>
2016-02-28 18:06:24 +0100

    patch: hardware: nozomi support (nozomi/hikari support together)

--- patch/hardware/broadcom/libbt/Android.mk
+++ patch/hardware/broadcom/libbt/Android.mk
@@ -43,8 +43,11 @@ endif
 ifeq ($(TARGET_PRODUCT), full_wingray)
     include $(LOCAL_PATH)/conf/moto/wingray/Android.mk
 endif
+ifeq ($(TARGET_DEVICE), nozomi)  
+    include $(LOCAL_PATH)/conf/sony/nozomi/Android.mk  
+endif  
 ifeq ($(TARGET_DEVICE), hikari)  
     include $(LOCAL_PATH)/conf/sony/hikari/Android.mk  
-endif  
+endif
 
 endif # BOARD_HAVE_BLUETOOTH_BCM
