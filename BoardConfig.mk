561d65279064e4fd40c2ab2b17168391b7fa5a91
mickybart <mickybart@pygoscelis.org>
2016-08-01 14:52:53 -0400

    Superuser replaced with AppOps

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -158,12 +158,6 @@ BOARD_CUSTOM_BOOTIMG_MK := device/sony/nozomi/custom/custombootimg.mk
 # custom ota
 BOARD_CUSTOM_OTA_MK := device/sony/nozomi/custom/customota.mk
 
-# Superuser
-TARGET_NO_SUPERUSER := false
-ifneq ($(TARGET_NO_SUPERUSER),true)
-SUPERUSER_EMBEDDED := true
-endif
-
 # SELinux
 BOARD_SEPOLICY_DIRS += \
     device/sony/nozomi/sepolicy
