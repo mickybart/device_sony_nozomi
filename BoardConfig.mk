3168453239b562a831b1a3f1bfbec9e99bfeaece
mickybart <mickybart@pygoscelis.org>
2016-09-15 09:53:21 -0400

    BoardConfig: Malloc svelte (reduce memory usage but impact allocation speed)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,6 +34,9 @@ TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 COMMON_GLOBAL_CFLAGS += -DLEGACY_BLOB_COMPATIBLE
 
+# malloc
+MALLOC_SVELTE := true
+
 # Jack
 ifeq ($(ANDROID_JACK_VM_ARGS),)
 ANDROID_JACK_VM_ARGS := -Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4096m
