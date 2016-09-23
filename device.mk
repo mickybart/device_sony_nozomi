fe407a647230eae4410a67a3cbe0f9354343444b
mickybart <mickybart@pygoscelis.org>
2016-09-23 15:32:17 -0400

    device: nosdcard

--- device.mk
+++ device.mk
@@ -24,7 +24,7 @@ DEVICE_PACKAGE_OVERLAYS += device/sony/hikari/overlay
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
-#PRODUCT_CHARACTERISTICS := nosdcard
+PRODUCT_CHARACTERISTICS := nosdcard
 
 # kernel
 PRODUCT_PACKAGES += \
