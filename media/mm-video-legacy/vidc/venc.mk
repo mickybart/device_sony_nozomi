81233f0be8b0305c1494ab857fffe62ad553e8ad
mickybart <mickybart@pygoscelis.org>
2016-10-21 11:12:19 -0400

    media: update to match hardware/qcom/media (camcorder fix)

--- media/mm-video-legacy/vidc/venc.mk
+++ media/mm-video-legacy/vidc/venc.mk
@@ -35,6 +35,9 @@ endif
 ifeq ($(TARGET_USES_ION),true)
 libmm-venc-def += -DUSE_ION
 endif
+ifeq ($(TARGET_USES_MEDIA_EXTENSIONS),true)
+libmm-venc-def += -DUSE_NATIVE_HANDLE_SOURCE
+endif
 libmm-venc-def += -D_ANDROID_ICS_
 # ---------------------------------------------------------------------------------
 # 			Make the Shared library (libOmxVenc)
@@ -56,6 +59,7 @@ libmm-venc-inc      += frameworks/av/include/media/stagefright
 LOCAL_MODULE                    := libOmxVenc
 LOCAL_MODULE_TAGS               := optional
 LOCAL_CFLAGS                    := $(libmm-venc-def)
+LOCAL_CLANG                     := false
 LOCAL_C_INCLUDES                := $(libmm-venc-inc)
 
 LOCAL_SHARED_LIBRARIES    := liblog libutils libbinder libcutils \
