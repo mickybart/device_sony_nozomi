245e509f73087180da2a1b5f884915b455c18cc4
Milos Ratkovic <milosr@gmail.com>
2016-11-01 18:44:12 +0100

    ota: added backuptool

--- custom/customota.mk
+++ custom/customota.mk
@@ -19,6 +19,7 @@ $(INTERNAL_OTA_PACKAGE_TARGET): $(BUILT_TARGET_FILES_PACKAGE) $(DISTTOOLS)
 	   -p $(HOST_OUT) \
 	   -k $(KEY_CERT_PAIR) \
 	   --no_separate_recovery=true \
+	   --backup=true \
 	   --custom_recovery_partition=/dev/block/mmcblk0p11 \
 	   $(BUILT_TARGET_FILES_PACKAGE) $@
 
