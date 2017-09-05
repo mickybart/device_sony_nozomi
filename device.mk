05b818d4442736fb21eadb768a899a74695838c4
Demon000 <demonsingur@gmail.com>
2017-09-05 09:11:40 +0300

    CameraWrapper: Store user pointer and pass it when needed
    The new camera interface calls set_callbacks with a pointer
    to it's internal CameraDevice and expects following calls to
    the callbacks to use that pointer.
    
    Store the pointer in the camera wraper and intercept the
    callbacks calls to pass it along.
    
    Change-Id: I99f02484e12a3f72cf1be13f1c724f474a452d7f

--- device.mk
+++ device.mk
@@ -161,8 +161,8 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     android.hardware.power@1.0-impl \
     android.hardware.keymaster@3.0-impl \
-    android.hardware.camera.provider@2.4-impl-legacy \
-    camera.device@1.0-impl-legacy
+    android.hardware.camera.provider@2.4-impl \
+    camera.device@1.0-impl
 
 DEVICE_MANIFEST_FILE := device/sony/nozomi/manifest.xml
 
