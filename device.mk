88a96a6c7f8c89e74ae8cded09d3e04faa65748f
mickybart <mickybart@pygoscelis.org>
2017-10-30 11:53:32 -0400

    Audio: fix voiceprocessing and re-enable it

--- device.mk
+++ device.mk
@@ -63,11 +63,11 @@ PRODUCT_PACKAGES += \
     audio.r_submix.default \
     libaudio-resampler
 
-## Audio Effects
-#PRODUCT_PACKAGES += \
+# Audio Effects
+PRODUCT_PACKAGES += \
+    libqcomvoiceprocessing
 #    ViPER4Android \
 #    libv4a_fx_ics \
-#    libqcomvoiceprocessing
 
 # Stlport for proprietary Adreno OpenGL lib
 PRODUCT_PACKAGES +=  \
