1c0ae0ef46b642d215bfd5d2635e4b885ee84dfe
Milos Ratkovic <milosr@gmail.com>
2017-11-26 14:55:03 +0100

    hidl: added keymaster

--- device.mk
+++ device.mk
@@ -154,6 +154,7 @@ PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service \
     android.hardware.sensors@1.0-impl \
     android.hardware.power@1.0-impl \
+    android.hardware.keymaster@3.0-impl \
     android.hardware.camera.provider@2.4-impl \
     camera.device@1.0-impl
 
