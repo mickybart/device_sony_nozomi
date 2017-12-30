e3743a64d9d2e000c7f5ecccdfda111ff7b5c03a
Artem Borisov <dedsa2002@gmail.com>
2017-12-30 16:03:20 +0300

    build: Enable boot and system server dex-preopt
    As stated in https://android.googlesource.com/platform/build/+/418258c
    this is required in O-MR1 since sepolicy now prevents system server from
    loading anything from /data making our system extremely slow and janky.
    
    Change-Id: I215ec7ebe95981ef0b953fe938e12b83675795ab

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -107,15 +107,15 @@ TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
 # charger/healthd
 BOARD_CHARGER_ENABLE_SUSPEND := true
 
-# Enable dex-preoptimization to speed up first boot sequence
+# Dexpreopt
 ifeq ($(HOST_OS),linux)
-  ifeq ($(TARGET_BUILD_VARIANT),user)
+  ifneq ($(TARGET_BUILD_VARIANT),eng)
     ifeq ($(WITH_DEXPREOPT),)
       WITH_DEXPREOPT := true
+      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
     endif
   endif
 endif
-DONT_DEXPREOPT_PREBUILTS := true
 
 TARGET_NO_BOOTLOADER := true
 TARGET_NO_RECOVERY := false
